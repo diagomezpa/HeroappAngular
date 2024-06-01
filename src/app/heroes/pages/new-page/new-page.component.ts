82a436de654b4f79b17d0d3b13d426841afc2175
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-06-01 07:10:27 -0500

    clase 15 video 211 ReactiveForms intro

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -6,6 +8,18 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class NewPageComponent {
+  public heroForm = new FormGroup({
+    // crea el formulario reactivo
+
+    id: new FormControl(''), // aca crea loas propiedades que quiero que maneje el formulario
+    superhero: new FormControl('', { nonNullable: true }),
+    publisher: new FormControl(''),
+    alter_ego: new FormControl<Publisher>(Publisher.DCComics),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
+
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
