899acaf0388899e92b1b75f2f8a56238a68a33d6
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-06-07 11:11:28 -0500

    clase 16 video suthsevice servicio de autenticacion

---
+++ src/app/auth/services/auth.services.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../interfaces/user.interface';
+import { Observable, tap } from 'rxjs';
+import { environments } from '../../../environments/environments';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private baseUrl = environments.baseUrl;
+  private user?: User;
+
+  constructor(private http: HttpClient) {}
+
+  get currentUser(): User | undefined {
+    if (!this.user) return undefined;
+
+    return structuredClone(this.user); // structuredclone crea un clo ya que no se puede pasar por referencia
+  }
+
+  login(email: string, password: string): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/users/1`).pipe(
+      tap((user) => (this.user = user)),
+      tap((user) => localStorage.setItem('token', user.id.toString()))
+    );
+  }
+}
