e687dd923f3ce702261748086faeea0478ad7374
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-06-06 21:42:24 -0500

    clase 15 video 217 material dialog

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -5,6 +5,8 @@ import { HeroesService } from '../../services/heroes.services';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-new-page',
@@ -33,7 +35,8 @@ export class NewPageComponent implements OnInit {
     private heroesService: HeroesService,
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private snackbar: MatSnackBar
+    private snackbar: MatSnackBar,
+    private dialog: MatDialog
   ) {}
 
   get currentHero(): Hero {
@@ -68,6 +71,34 @@ export class NewPageComponent implements OnInit {
       // TODO: mostrar snackbar y navegar a /heroes/edit/ hero.id
       this.router.navigate(['/heroes/edit', hero.id]);
       this.showSnackbar(`${hero.superhero} created!`);
+    });
+  }
+  onDeleteHero() {
+    if (!this.currentHero.id) throw Error('Hero id is required');
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: this.heroForm.value,
+    });
+
+    // dialogRef
+    //   .afterClosed()
+    //   .pipe(
+    //     filter((result: boolean) => result),
+    //     switchMap(() => this.heroesService.deleteHeroById(this.currentHero.id)),
+    //     filter((wasDeleted: boolean) => wasDeleted)
+    //   )
+    //   .subscribe(() => {
+    //     this.router.navigate(['/heroes']);
+    //   });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (!result) return; // esta cosa retorna falso verdadero o undefinned
+
+      this.heroesService
+        .deleteHeroById(this.currentHero.id)
+        .subscribe((wasDeleted) => {
+          if (wasDeleted) this.router.navigate(['/heroes']);
+        });
     });
   }
 
