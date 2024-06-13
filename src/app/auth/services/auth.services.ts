569cfe803d1549b292a925c4cdd52449c06230ef
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-06-13 12:55:53 -0500

    clase 16 video 225 mantener la sesión

--- src/app/auth/services/auth.services.ts
+++ src/app/auth/services/auth.services.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Pipe } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of, pipe, tap } from 'rxjs';
 import { environments } from '../../../environments/environments';
 
 @Injectable({ providedIn: 'root' })
@@ -24,6 +24,18 @@ export class AuthService {
       tap((user) =>
         localStorage.setItem('token', 'aASDgjhasda.asdasd.aadsf123k')
       )
+    );
+  }
+
+  checkAuthentication(): Observable<boolean> {
+    if (!localStorage.getItem('token')) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`).pipe(
+      tap((user) => (this.user = user)),
+      map((user) => !!user),
+      catchError((err) => of(false))
     );
   }
 
